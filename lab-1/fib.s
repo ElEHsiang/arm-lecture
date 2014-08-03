c162596c18728efc89c58bfcfe228be380432529
elehsiang <elehsiang>
2014-08-03 16:42:47 +0800

    finish lab1

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -15,18 +15,30 @@ fibonacci:
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+    mov r4, r0
+    cmp r4, #0
+    ble .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+    subs r4, r4, #1
+    beq .L4
 
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
+    mov r0, r4
+    bl fibonacci
+    mov r5,r0
 
 	@ R5 = R0
 	@ R0 = R4 - 2
 	@ Recursive call to fibonacci with R4 - 2 as parameter
+    sub r4, r4, #1
+    mov r0, r4
+    bl fibonacci
 
 	@ R0 = R5 + R0 (update flags)
+    adds r0, r5, r0
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
